274b42e66c303b5f79abdb62838bf86e4abf978a
AntonZ-Frank <toni.rus@yandex.ru>
2020-02-20 16:01:36 +0500

    first

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 1000;
+        double priceMile = 20;
+        double totalMile = ticket / priceMile;
+        System.out.println(totalMile);
+    }
+}
